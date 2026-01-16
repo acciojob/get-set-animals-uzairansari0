bad102c040c217ec0e8eb30852a3e1822fa5dc21
Uzair <109786747+uzairansari0@users.noreply.github.com>
2026-01-16 16:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
-	get species(){
-		return this._species
-	}
-	makeSound(){
-		console.log(`The ${this.species} makes a sound`)
-	}
-}
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {
-	constructor(){
-		 super("Car");
-	}
-	bark(){
-		console.log('woof')
-	}	
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
 
 class Cat extends Animal {
-	constructor(){
-		 super("Dog");
-	}
-	purr(){
-		console.log('purr')
-	}
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+	  super(species);
+  }
+
+  bark() {
+    console.log("woof");
+  }
 }
 
 // Do not change the code below this line
